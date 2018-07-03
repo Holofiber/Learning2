eab92be5225a6b9b266bee49c0fd2c1006b85804
SProts <aminus2010@gmail.com>
2018-07-03 23:54:30 +0300

    Learn :: First commit

---
+++ Learning/AnonymMethAndLamda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymMethAndLamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
