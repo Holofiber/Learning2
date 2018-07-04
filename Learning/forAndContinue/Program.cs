5016491f8caba1d85261c5cd50fc587f1e37d4f1
SProts <aminus2010@gmail.com>
2018-07-04 15:51:17 +0300

    Learn :: test for with continue

--- Learning/forAndContinue/Program.cs
+++ Learning/forAndContinue/Program.cs
@@ -7,6 +7,18 @@ namespace forAndContinue
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            for (int i=0; i<10;i++)
+            {
+                Console.WriteLine("text "+i);
+                if(i<9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("some...");
         }
     }
 }
