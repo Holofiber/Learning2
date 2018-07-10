a197785370301313e110cdcc33b91e1e8f2d9010
SProts <aminus2010@gmail.com>
2018-07-10 16:06:18 +0300

    Learn :: Show MouseMove
    Related Work Items: #163

---
+++ Learning/mouseCordinates/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouseCordinates
+{
+    public partial class Form1 : Form
+    {
+        private int x = 0;
+        private int y = 0;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.Location;
+            var x = e.X;
+            var y = e.Y;
+            Text = position.ToString();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var dataObject = e.Data;
+            
+        }
+    }
+}
