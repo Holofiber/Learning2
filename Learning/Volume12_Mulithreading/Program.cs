e63a61294d47c26d9dd23c59aa017586af7d5eba
Stepan Prots <stepan.prots@eleks.com>
2022-03-22 21:36:01 +0100

    Learning: add git ignore

---
+++ Learning/Volume12_Mulithreading/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//public class QueueProcessor
+//{
+//    public QueueProcessor(Queue theQueue)
+//    {
+//        this.theQueue = theQueue;
+//        theThread = new Thread(new ThreadStart(this.ThreadFunc));
+//    }
+
+//    private Queue theQueue;
+//    private Thread theThread;
+//    private Thread thread;
+
+//}
+
+public class EntrPoint
+{
+    private static void ThreadFunc()
+    {
+        Console.WriteLine($"hello from thread {Thread.CurrentThread.GetHashCode()}");
+    }
+
+    
+
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(new ThreadStart(EntrPoint.ThreadFunc));
+        Console.WriteLine($"main thread {Thread.CurrentThread.ManagedThreadId}");
+
+        Console.WriteLine("run new thread...");
+
+        thread.Start();
+
+        thread.Join();
+        Console.WriteLine("thread finished");
+    }
+}
