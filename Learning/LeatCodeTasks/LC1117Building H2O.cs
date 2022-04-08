77f3c769419449f600f3b06b2b0515e80c46c02a
Stepan Prots <stepan.prots@eleks.com>
2022-04-08 20:45:18 +0200

    LeatCode: 1117Building_H2O

---
+++ Learning/LeatCodeTasks/LC1117Building H2O.cs
@@ -0,0 +1,53 @@
+﻿namespace LeatCodeTasks
+{
+    public class LC1117Building_H2O
+    {
+        public LC1117Building_H2O(int count)
+        {
+            GenerateTask(count);
+        }
+
+        private void GenerateTask(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Thread t = new Thread(Hidro);
+                t.Start();
+            }
+
+            for (int i = 0; i < count/2; i++)
+            {
+                Thread t = new Thread(Oxy);
+                t.Start();
+            }
+        }
+
+        private static Semaphore H = new Semaphore(2, 2);
+        private static Semaphore O = new Semaphore(0, 2);
+
+        object syncLock = new object();
+
+        public void Hidro()
+        {
+            H.WaitOne();
+
+            Console.WriteLine($"Hidro");
+
+            O.Release();
+        }
+
+
+        public void Oxy()
+        {
+            lock (syncLock)
+            {
+                O.WaitOne();
+                O.WaitOne();
+                Console.WriteLine($"Oxy ");
+
+                H.Release(2);
+            }
+        }
+
+    }
+}
---
+++ Learning/LeatCodeTasks/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using LeatCodeTasks;
+
+Console.WriteLine("Leat Code Tasks sandbox \n");
+
+Console.WriteLine("Taks name \"LC1117Building_H2O\" \n");
+LC1117Building_H2O lC1117Building_H2O = new LC1117Building_H2O(20);
+
+Console.ReadKey();
