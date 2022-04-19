9c9c731b3ce151a09e13c8d5c9e7faa0cd8a685b
Stepan Prots <stepan.prots@eleks.com>
2022-04-19 21:51:04 +0200

    SandBox: ConcurrentQueue, workers

--- Learning/SandBoxConsole/Program.cs
+++ Learning/SandBoxConsole/Program.cs
@@ -1,43 +1,93 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Numerics;
+using System.Drawing;
+using System.Threading;
 using System.Threading.Tasks;
+using Console = Colorful.Console;
 
 namespace SandBoxConsole
 {
     public class Program
     {
-        const int FactorialToCompute = 4000;
-
+        public delegate bool SomeDelegate();
         static void Main(string[] args)
         {
-            var numbers = new ConcurrentDictionary<BigInteger, BigInteger>(4, FactorialToCompute);
+            Run();
 
-            //Func<BigInteger, BigInteger> factorial = null;
-            //factorial = (n) => (n == 0) ? 1 : n * factorial(n - 1); 
-
-            BigInteger factorial(BigInteger n)
+            while (true)
             {
-                return (n == 0) ? 1 : n * factorial(n - 1);
+                var data = Console.ReadLine();
+
+                if (data != null)
+                {
+                    queue.Enqueue(Convert.ToInt32(data));
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        public static ConcurrentQueue<int> queue = GetQueue();
+
+        public static async void Run()
+        {
+            Worker workerRed = new Worker(queue, 2000, Color.Red);
+            Worker workerGreen = new Worker(queue, 1000, Color.Green);
+
+            Task t1 = Task.Factory.StartNew(() => workerRed.Start());
+
+            Task t2 = Task.Factory.StartNew(() => workerGreen.Start());
+
+            Thread.SpinWait(100000000);
+            workerGreen.Stop();
+        }
+
+        public static ConcurrentQueue<int> GetQueue()
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            Random random = new Random(300);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                queue.Enqueue(i);
             }
 
-            var po = new ParallelOptions();
+            return queue;
+        }
+    }
 
-            po.MaxDegreeOfParallelism = Environment.ProcessorCount;
+    public class Worker
+    {
+        private readonly ConcurrentQueue<int> queue;
+        private readonly int pending;
+        private readonly Color color;
+        private bool run = true;
 
-            //set GC to server mode for better performens.
-            Parallel.For(0, FactorialToCompute, po, (i) =>
+        public Worker(ConcurrentQueue<int> queue, int pending, Color color)
+        {
+            this.queue = queue;
+            this.pending = pending;
+            this.color = color;
+        }
+
+        public void Start()
+        {
+            System.Console.WriteLine($"ID: {Thread.CurrentThread.ManagedThreadId}", Color.Yellow);
+
+            while (run)
             {
-                numbers[i] = factorial(i);
-            });
+                Thread.Sleep(pending);
 
-            Console.WriteLine("DONE");
+                if (queue.TryDequeue(out int num))
+                    Console.WriteLine($"{num * num}", color);
+            }
 
-            //for (ulong i = 0; i < FactorialToCompute; ++i)
-            //{
-            //    numbers[i] = factorial(i);
-            //}
+            System.Console.WriteLine("end");
+        }
 
+        public void Stop()
+        {
+            run = false;
         }
     }
 }
